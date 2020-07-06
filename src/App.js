8f2e196bd4b65db900e0294c71b3a4309729992f
mahdihrs <mahdi.haris@myboost.id>
2020-07-06 20:08:38 +0700

    new boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import aing from './assets/me-lofi.jpeg'
+
+function App() {
+  return (
+    <>
+      <img src={aing} alt="gambar aing" />
+    </>
+  );
+}
+
+export default App;
